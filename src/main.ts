574ddf8869b0f7c5767c9ffba9866ac0b43848ee
Abdurrahman Yildirim <abdurrahman.yildirim@etalytics.com>
2024-08-11 21:13:54 +0200

    clean up

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import { Routes } from '@angular/router';
-
-bootstrapApplication(
-  AppComponent,
-
-  {
-    providers: [
-      provideZoneChangeDetection({ eventCoalescing: true }),
-      provideRouter([]),
-      provideAnimationsAsync(),
-    ],
-  },
-).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideAnimationsAsync()],
+}).catch((err) => console.error(err));
